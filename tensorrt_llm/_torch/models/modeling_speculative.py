bd53ff81e59dbcb38508e2cbf05c621e446c84f7
Izzy Putterman <iputterman@nvidia.com>
2026-01-31 22:58:06 -0800

    More attempts to fix
    Signed-off-by: Izzy Putterman <iputterman@nvidia.com>

--- tensorrt_llm/_torch/models/modeling_speculative.py
+++ tensorrt_llm/_torch/models/modeling_speculative.py
@@ -918,6 +918,7 @@ class SpecDecOneEngineForCausalLM(DecoderModelForCausalLM[TModel, TConfig],
         self.draft_model = None
         self.draft_config = None
         spec_config = getattr(model_config, 'spec_config', None)
+        self.spec_config = spec_config
         if spec_config and spec_config.spec_dec_mode.use_one_engine():
             if spec_config.spec_dec_mode.is_eagle3_one_model():
                 if spec_config.eagle3_model_arch == "mistral_large3":
@@ -1056,8 +1057,7 @@ class SpecDecOneEngineForCausalLM(DecoderModelForCausalLM[TModel, TConfig],
                            weight_mapper: Optional[BaseWeightMapper] = None):
         self.draft_model.load_weights(weights=weights,
                                       weight_mapper=weight_mapper)
-        spec_config = getattr(self.config, 'spec_config', None)
-        if spec_config and not spec_config.spec_dec_mode.is_draft_target_one_model(
+        if self.spec_config and not self.spec_config.spec_dec_mode.is_draft_target_one_model(
         ):
             self.draft_model.load_weights_from_target_model(self)
 
